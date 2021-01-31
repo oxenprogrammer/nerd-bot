c3f9de9d8913f24753065b455a94561f26045ff5
oxenprogrammer <emmy1000okello@gmail.com>
2021-01-31 15:39:57 +0300

    config(setup): add heroku

--- bin/bot.rb
+++ bin/bot.rb
@@ -3,3 +3,31 @@
 require 'slack-ruby-bot'
 require_relative './../lib/command/slack_command'
 require_relative './../lib/slack-bot'
+
+require 'httparty'
+require 'nokogiri'
+class Web
+  def initialize
+    @base_url = 'https://www.hongkiat.com/blog/programming-jokes/'
+    unparsed_page = HTTParty.get(@base_url)
+    @parsed_page = Nokogiri::HTML(unparsed_page.body)
+
+    @text_url = 'https://upjoke.com/programmer-jokes'
+    unparsed_page_text = HTTParty.get(@text_url)
+    @parsed_page_text = Nokogiri::HTML(unparsed_page_text.body)
+  end
+
+  def images
+    imgs = @parsed_page.css('figure.entry-image img[src]').select { |image| image['src'].start_with?('https') }
+    puts imgs.sample
+  end
+
+  def text_jokes
+    my_jokes  = @parsed_page_text.css('.joke-wrapper').map(&:text)
+    puts my_jokes.sample
+  end
+end
+
+web = Web.new
+web.images
+
--- lib/command/slack_command.rb
+++ lib/command/slack_command.rb
@@ -4,16 +4,12 @@ require 'nokogiri'
 module JokerTheNerd
   module Commands
     class Jokes < SlackRubyBot::Commands::Base
-      def initialize
-        super
-        @base_url = 'https://www.https://www.hongkiat.com/blog/programming-jokes/'
-        unparsed_page = HTTParty.get(@base_url)
-        @parsed_page = Nokogiri::HTML(unparsed_page.body)
+      command 'jokes_image' do |client, data, _match|
+        client.say(channel: data.channel, text: JokesImage.image_jokes)
       end
 
-      command 'get_all_humors' do |client, data, _match|
-        first_humor = @parsed_page.css('figure.entry-image img').find { images }
-        client.say(channel: data.channel, image: first_humor)
+      command 'jokes_text' do |client, data, _match|
+        client.say(channel: data.channel, text: JokesText.text_jokes)
       end
 
       command 'say_hello' do |client, data, _match|
@@ -25,6 +21,26 @@ module JokerTheNerd
   class HelloText
     def self.say_hello
       'Hello, Joker the Nerd Here'
+    end
+  end
+
+  class JokesText
+    def self.text_jokes
+      @text_url = 'https://upjoke.com/programmer-jokes'
+      unparsed_page_text = HTTParty.get(@text_url)
+      @parsed_page_text = Nokogiri::HTML(unparsed_page_text.body)
+      my_jokes = @parsed_page_text.css('.joke-wrapper').map(&:text)
+      my_jokes.sample
+    end
+  end
+
+  class JokesImage
+    def self.image_jokes
+      @base_url = 'https://www.hongkiat.com/blog/programming-jokes/'
+      unparsed_page = HTTParty.get(@base_url)
+      @parsed_page = Nokogiri::HTML(unparsed_page.body)
+      imgs = @parsed_page.css('figure.entry-image img[src]').select { |image| image['src'].start_with?('https') }
+      imgs.sample
     end
   end
 end
