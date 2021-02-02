4ec5e96f9746269d156279993ab566179bd3630f
oxenprogrammer <emmy1000okello@gmail.com>
2021-02-02 03:17:38 +0300

    feat(slack-bot): update help commands

--- lib/slack_bot.rb
+++ lib/slack_bot.rb
@@ -8,19 +8,19 @@ module Joker
       desc 'This bot serves you with some programming humors'
 
       command :say_hello do
-        title 'Greet the Bot'
+        title 'say_hello'
         desc 'The Bot greets back'
         long_desc 'The bot responds with a greeting string'
       end
 
       command :jokes_text do
-        title 'Joke Text'
+        title 'jokes_text'
         desc 'Get random Programming Jokes as Text'
         long_desc 'The Bot craws and scraps the web, returning a random joke texts'
       end
 
       command :jokes_image do
-        title 'Joke Image'
+        title 'jokes_image'
         desc 'Get random Programming Jokes as Image'
         long_desc 'The Bot craws and scraps the web, returning a random joke image'
       end
